fe6411adca5ee8c284582912a3fb6676ce3e10a0
Mads A. Pedersen <mads.amtoft@hotmail.com>
2024-12-19 23:48:13 +0100

    Figuring it out

--- Day1/HistorianHysteria.cpp
+++ Day1/HistorianHysteria.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
+#include <fstream>
+#include <list>
 
 int main() {
-    int locationID = 0;
+    std::list<int> locationList1;
+    std::list<int> locationList2;
+
+    std::ifstream inFile;
+
+    inFile.open("input.txt");
+
+    long long t;
+
+    inFile >> t;
+
+    std::cout << "t: " << t << std::endl;
 
 
 
